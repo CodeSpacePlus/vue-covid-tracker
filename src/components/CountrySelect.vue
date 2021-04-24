243e482fb55cee1963810a13f8778de16e900da1
Bryan Chavez <bryanc@gmail.com>
2021-04-24 11:22:35 -0700

    covid tracker

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,36 @@
+<template>
+	<select
+		class="form-select mt-10 block w-full border p-3 rounded"
+		v-model="selected"
+		@change="onChange"
+	>
+		<option value="0">Select Country</option>
+		<option
+			:value="country.ID"
+			v-for="country in countries"
+			:key="country.ID"
+			>{{ country.Country }}</option
+		>
+	</select>
+</template>
+
+<script>
+	export default {
+		name: "CountrySelect",
+		props: ["countries"],
+		data() {
+			return {
+				selected: 0,
+			};
+		},
+		methods: {
+			onChange() {
+				const country = this.countries.find(
+					(item) => item.ID === this.selected
+				);
+
+				this.$emit("get-country", country);
+			},
+		},
+	};
+</script>
